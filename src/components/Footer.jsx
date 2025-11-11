4a9bc876a1a110ce3acd5f6026f592f763c794f9
Prithviraj Biswas <prithviraj.biswas@accenture.com>
2025-11-11 18:16:18 +0530

    css changes and name added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,22 @@ const Footer = () => {
       <div className="footer-container">
         {" "}
         <p className="footer-text">
-          © The website is built by{" "}
+          © Website designed and maintained by{" "}
+          <strong>
+            <a
+              href="https://www.linkedin.com/in/prithviraj-biswas/"
+              target="_blank"
+            >
+              Prithviraj Biswas{" "}
+            </a>
+          </strong>
+          ,{" "}
           <strong>
             <a
               href="https://www.linkedin.com/in/debjit-kushari-7ba543246/"
               target="_blank"
             >
-              Debjit Kushari{" "}
+               Debjit Kushari{" "}
             </a>
           </strong>
           and
